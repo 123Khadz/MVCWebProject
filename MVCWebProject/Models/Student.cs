867be4bb060da97b904c1c8ffa7fb60e636c937e
khadi <khadi@DESKTOP-J7PQMCI>
2022-03-06 19:15:53 +0530

    Final

--- MVCWebProject/Models/Student.cs
+++ MVCWebProject/Models/Student.cs
@@ -7,5 +7,9 @@ namespace MVCWebProject.Models
 {
     public class Student
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public float Mark { get; set; }
     }
 }
