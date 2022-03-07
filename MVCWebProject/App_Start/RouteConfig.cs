8a1dfc47e03b534f6a15666abf41181ad0d7ec48
khadi <khadi@DESKTOP-J7PQMCI>
2022-03-07 18:03:59 +0530

    HtmlHelper

--- MVCWebProject/App_Start/RouteConfig.cs
+++ MVCWebProject/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace MVCWebProject
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Employee",
+                url: "employee/{action}/{id}",
+                defaults: new { controller = "Employee", action = "Name", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ MVCWebProject/Controllers/HtmlHelperDemoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebProject.Controllers
+{
+    public class HtmlHelperDemoController : Controller
+    {
+        // GET: HtmlHelperDemo
+        public ActionResult Index()
+        {
+            ViewBag.Courses = new string[] { "C#", "Java", "Php", "MS.Net", "Python" };
+            return View();
+        }
+
+        public ViewResult StandardHelper()
+        {
+            ViewBag.Qualification = new string[] { "BCA", "MCA", "BTech", "MTech", "MBA" };
+            ViewBag.City = new SelectList(new string[] { "Noida", "Delhi", "Patna", "Gaya", "Lucknow" });
+            return View();
+        }
+
+        [HttpPost]
+        public string StandardHelper(FormCollection form)
+        {
+            return "Details:" + form["Id"] + "," + form["Name"] + "," + form["gender"] + "," + form["City"];
+        }
+
+        public ViewResult TemplatedHelper()
+        {
+            return View();
+        }
+    }
+}
